2f2098a2775d9af40a1667a7c2bb4bb09f73c480
PierrickPochelu <44296854+PierrickPochelu@users.noreply.github.com>
2019-06-02 17:25:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,10 @@ import tensorflow as tf
 
 epoch=200
 batch_size=8
+split=0.5
+nb_experiments=30
 
-# create X
-nf=np.random.uniform((0.5,-0.5),(1.5,0.5),(100,2)) #1,0
-f1=np.random.uniform((-0.5,-0.5),(0.5,0.5),(100,2)) # 0,0
-f2=np.random.uniform((-0.5,0.5),(0.5,1.5),(100,2)) # 0,1
-f3=np.random.uniform((-0.5,1.5),(0.5,2.5),(100,2)) #0,2
-xtrain=np.concatenate((nf,f1,f2,f3))
+
 
 
 def optional_categorical_crossentropy(target, output, from_logits=False):
@@ -32,30 +29,11 @@ def optional_categorical_crossentropy(target, output, from_logits=False):
         return tf.nn.softmax_cross_entropy_with_logits(labels=target,
                                                        logits=output)
 
-# create Y
-class1= [np.array([1,0]),np.array([-1,-1,-1])]
-class2= [np.array([0,1]),np.array([1,0,0])]
-class3= [np.array([0,1]),np.array([0,1,0])]
-class4= [np.array([0,1]),np.array([0,0,1])]
-ynf=np.array([ class1 for i in range(100)])
-yf1=np.array([ class2 for i in range(100)])
-yf2=np.array([ class3 for i in range(100)])
-yf3=np.array([ class4 for i in range(100)])
-ytrain=np.concatenate((ynf,yf1,yf2,yf3))
-
-# shuffle
-ids=np.array(range(xtrain.shape[0]))
-np.random.shuffle(ids)
-xtrain=xtrain[ids]
-ytrain=ytrain[ids]
-
-split=0.5
-split_id=int(xtrain.shape[0]*split)
-xtest=xtrain[:split_id]
-xtrain=xtrain[split_id:]
-ytest=ytrain[:split_id]
-ytrain=ytrain[split_id:]
-
+def reformat_to_multi_output(y):
+    y_double=[]
+    y_double.append( np.array([ysample[0]  for ysample in y ]) )
+    y_double.append( np.array([ysample[1]  for ysample in y ]) )
+    return y_double
 
 
 def get_model(is_out1=True, is_out2=True):
@@ -85,33 +63,110 @@ def get_model(is_out1=True, is_out2=True):
 
     return model
 
-print(xtrain.shape)
-
-# reformat Y
-ytrain_double=[]
-ytrain_double.append( np.array([ysample[0]  for ysample in ytrain ]) )
-ytrain_double.append( np.array([ysample[1]  for ysample in ytrain ]) )
-ytest_double=[]
-ytest_double.append( np.array([ysample[0]  for ysample in ytest ]) )
-ytest_double.append( np.array([ysample[1]  for ysample in ytest ]) )
+def compute_weights_for_CE(y):
+    first_sample=0
+    nboutputs=y.shape[1]
+    softmax_size=[]
+    for i in range(nboutputs):
+        nb_class_in_this_out=y[first_sample][i].shape[0]
+        softmax_size.append(nb_class_in_this_out)
 
 
-assert(len(ytrain_double)==2)
-assert(ytrain_double[0].shape[1]==2)
-assert(ytrain_double[1].shape[1]==3)
+    # compute frequencies
+    tree=[]
+    for i in range(nboutputs): # for each softmax i
+        y_output_i=np.array(list(y[:,i]))
 
-# all together
-model = get_model(True, True)
-model.fit(x=xtrain,y=ytrain_double,batch_size=batch_size,epochs=epoch,verbose=0
-          )
-print(model.evaluate(xtest,ytest_double))
+        nb_minus_1=np.zeros(softmax_size[i])
+        nb_0=np.zeros(softmax_size[i])
+        nb_1 = np.zeros(softmax_size[i])
+        for j in range(softmax_size[i]): # for each ouput j in softmax i
+            nb_minus_1[j]=(y_output_i[:,j]==-1).sum()
+            nb_0[j] = (y_output_i[:,j] == 0).sum()
+            nb_1[j] = (y_output_i[:,j] == 1).sum()
 
-# loss 1 only
-model = get_model(True, False)
-model.fit(x=xtrain,y=ytrain_double[0],batch_size=batch_size,epochs=epoch,verbose=0)
-print(model.evaluate(xtest,ytest_double[0]))
+        res_i=nb_1/(nb_1+nb_0)
+        tree.append( res_i )
 
-# loss 2 only
-model = get_model(False, True)
-model.fit(x=xtrain,y=ytrain_double[1],batch_size=batch_size,epochs=epoch,verbose=0)
-print(model.evaluate(xtest,ytest_double[1]))
+    # compute weights
+    #scale=1./np.sum(tree[i],axis=0)
+    for i in range(nboutputs):
+        tree[i] = (1./tree[i])
+
+    return tree
+
+
+def get_synthetic_dataset():
+    # create X
+    nf=np.random.uniform((0.5,-0.5),(1.5,0.5),(900,2)) #1,0
+    f1=np.random.uniform((-0.5,-0.5),(0.5,0.5),(100,2)) # 0,0
+    f2=np.random.uniform((-0.5,0.5),(0.5,1.5),(100,2)) # 0,1
+    f3=np.random.uniform((-0.5,1.5),(0.5,2.5),(100,2)) #0,2
+    x=np.concatenate((nf,f1,f2,f3))
+
+    # create Y
+    class1= [np.array([1,0]),np.array([-1,-1,-1])]
+    class2= [np.array([0,1]),np.array([1,0,0])]
+    class3= [np.array([0,1]),np.array([0,1,0])]
+    class4= [np.array([0,1]),np.array([0,0,1])]
+    ynf=np.array([ class1 for i in range(900)])
+    yf1=np.array([ class2 for i in range(100)])
+    yf2=np.array([ class3 for i in range(100)])
+    yf3=np.array([ class4 for i in range(100)])
+    y=np.concatenate((ynf,yf1,yf2,yf3))
+
+    # shuffle
+    ids=np.array(range(x.shape[0]))
+    np.random.shuffle(ids)
+    y=y[ids]
+
+    # split train/test
+    split_id=int(x.shape[0]*split)
+    xtest=x[:split_id]
+    xtrain=x[split_id:]
+    ytest=y[:split_id]
+    ytrain=y[split_id:]
+
+    weights=compute_weights_for_CE(ytrain) # Warning : keras does not understand weights in multi-softmax context
+
+    # reformat Y
+    ytrain_double=reformat_to_multi_output(ytrain)
+    ytest_double=reformat_to_multi_output(ytest)
+
+
+
+    return xtrain, ytrain_double, xtest, ytest_double,weights
+
+
+if __name__=="__main__":
+    xtrain, ytrain_double, xtest, ytest_double, w = get_synthetic_dataset()
+    print(w)
+
+    assert(len(ytrain_double)==2)
+    assert(ytrain_double[0].shape[1]==2)
+    assert(ytrain_double[1].shape[1]==3)
+
+    # all together
+    ev=np.array([0.,0.,0.],dtype=float)
+    for i in range(nb_experiments):
+        model = get_model(True, True)
+        model.fit(x=xtrain,y=ytrain_double,batch_size=batch_size,epochs=epoch,verbose=0)
+        ev+=model.evaluate(xtest,ytest_double)
+    print(ev/nb_experiments)
+
+
+    # loss 1 only
+    ev=0
+    for i in range(nb_experiments):
+        model = get_model(True, False)
+        model.fit(x=xtrain,y=ytrain_double[0],batch_size=batch_size,epochs=epoch,verbose=0)
+        ev+=model.evaluate(xtest,ytest_double[0])
+    print(ev/nb_experiments)
+
+    # loss 2 only
+    ev=0
+    for i in range(nb_experiments):
+        model = get_model(False, True)
+        model.fit(x=xtrain,y=ytrain_double[1],batch_size=batch_size,epochs=epoch,verbose=0)
+        ev+=model.evaluate(xtest,ytest_double[1])
+    print(ev/nb_experiments)
